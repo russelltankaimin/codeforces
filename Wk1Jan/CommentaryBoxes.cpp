0143e0881b03c43437a7c7f4560a75f4d7ea6e93
Russell Tan <50812868+russelltankaimin@users.noreply.github.com>
2022-01-10 08:19:56 +0800

    Add files via upload

---
+++ Wk1Jan/CommentaryBoxes.cpp
@@ -0,0 +1,56 @@
+//#include "/Users/russelltankaimin/bits/stdc++.h"
+#include "bits/stdc++.h"
+/*
+#include<iostream>
+#include<algorithm>
+#include<iterator>
+#include<numeric>
+#include<map>
+#include<set>
+#include<bitset>
+#include<iomanip>
+#include<string>
+#include<vector>
+#include<deque>
+#include<array>
+#include<utility>
+#include<cmath>
+#include<cctype>
+*/
+using namespace std;
+typedef long long ll;
+typedef bitset<22> BITS;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+#define ALL(x) x.begin(),x.end()
+#define UNIQUE(x) (x).resize(unique(ALL(x)) - x.begin())
+template<typename T,typename U>
+void debug(pair<T,U> a, bool submit){
+	if(!submit){
+		cout <<"[ ";
+	}
+	for(int i = 0; i < a.size();i++){
+		cout << "(" << a[i].first << "," << a[i].second <<")" << " ";
+	}
+		cout <<(!submit ? " ]" : "")<< endl;
+}
+template<typename T>
+void debug(T a, bool submit){
+	if(!submit){
+		cout <<"[ ";
+	}
+	for(int i = 0; i < a.size();i++){
+		cout << a[i] << " ";
+	}
+		cout <<(!submit ? " ]" : "")<< endl;
+}
+
+void solve(){
+	ll n ,m , a ,b ; cin >> n >> m >> a >> b;
+	cout << min(n % m * b, (m - n % m) * a) << endl;
+}
+
+int main(){
+	solve();
+	return 0;
+}
